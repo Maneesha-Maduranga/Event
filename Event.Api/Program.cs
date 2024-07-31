8fe55ff1afc476fb78732e3232660f8c300d05c5
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-07-31 10:47:02 +0530

    Inject the dbcontext

--- Event.Api/Program.cs
+++ Event.Api/Program.cs
@@ -1,3 +1,6 @@
+using Event.Api.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("EventMasterDbConnection");
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
