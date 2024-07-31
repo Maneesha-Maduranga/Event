bb404b0973da047923f34cb0f48254e52e3360a5
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-07-31 10:27:58 +0530

    Create Event Model

---
+++ Event.Api/Models/Domain/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Event.Api.Models.Domain
+{
+    public class Event
+    {
+        public Guid id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Organizer { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+        public bool IsFull { get; set; }
+    }
+}
