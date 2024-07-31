203cea2f8d4787e37d9e80793ea615d6c2da4930
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-07-31 12:36:20 +0530

    Add Dto for eevntItem

---
+++ Event.Api/Models/Dto/EventItem/CreateEventItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Event.Api.Models.Dto.EventItem
+{
+    public class CreateEventItemDto
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }    
+        public string Organizer { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+        public bool IsFull { get; set; }
+    }
+}
