6cff51deb9b9bcdf5de42e9c1247a99360de09b2
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-07-31 12:36:52 +0530

    Implemt the repository for EventItem

---
+++ Event.Api/Repository/Implementation/EventItemRepository.cs
@@ -0,0 +1,71 @@
+﻿using Event.Api.Context;
+using Event.Api.Models.Domain;
+using Event.Api.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Event.Api.Repository.Implementation
+{
+    public class EventItemRepository : IEventItemRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EventItemRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<EventItem> CreateEventItem(EventItem eventItem)
+        {
+            await dbContext.AddAsync(eventItem);
+            await dbContext.SaveChangesAsync();
+            return eventItem;
+        }
+
+        public async Task<IEnumerable<EventItem>> GetAllEventItems()
+        {
+            return await dbContext.EventItems.ToListAsync();
+        }
+
+        public async Task<EventItem?> GetEventItem(Guid id)
+        {
+            return await dbContext.EventItems.FirstOrDefaultAsync(item => item.id == id);
+        }
+
+        public async Task<EventItem?> UpdateEventItem(EventItem eventItem)
+        {
+           var exEventItem = await dbContext.EventItems.FirstOrDefaultAsync(item => item.id == eventItem.id);
+           if (exEventItem != null)
+            {
+
+                exEventItem.Price = eventItem.Price;
+                exEventItem.Location = eventItem.Location;
+                exEventItem.Price = eventItem.Price;
+                exEventItem.Title = eventItem.Title;
+                exEventItem.Location = eventItem.Location;
+                exEventItem.Capacity = eventItem.Capacity;
+                exEventItem.Description = eventItem.Description;
+                eventItem.IsFull = eventItem.IsFull;
+
+               
+                dbContext.EventItems.Update(exEventItem);
+                await dbContext.SaveChangesAsync();
+                return exEventItem;
+            }
+
+            return null;
+        }
+
+        public async Task<EventItem?> DeleteEventItem(Guid id)
+        {
+           var isEventItem = await dbContext.EventItems.FirstOrDefaultAsync(item => item.id == id);
+
+           if (isEventItem != null)
+           {
+                dbContext.EventItems.Remove(isEventItem);
+                await dbContext.SaveChangesAsync();
+                return isEventItem;
+           }
+            return null; 
+        }
+    }
+}
---
+++ Event.Api/Repository/Interface/IEventItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using Event.Api.Models.Domain;
+
+namespace Event.Api.Repository.Interface
+{
+    public interface IEventItemRepository
+    {
+        Task<EventItem> CreateEventItem(EventItem eventItem);
+        Task<IEnumerable<EventItem>> GetAllEventItems();
+        Task<EventItem?> GetEventItem(Guid id);
+        Task<EventItem?> UpdateEventItem(EventItem eventItem);
+        Task<EventItem?> DeleteEventItem(Guid id);
+
+    }
+}
