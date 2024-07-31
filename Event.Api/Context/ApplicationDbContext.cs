3aeda4f498aa61efe16d256b8e1d317c20cd1d73
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-07-31 10:35:49 +0530

    Add Db context

---
+++ Event.Api/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Event.Api.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event.Api.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<EventItem> EventItems { get; set; }
+    }
+}
